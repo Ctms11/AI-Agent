eed036156d9a304a59be55d47a6d6bd86e9928ba
Ctms11 <clgraves2000@gmail.com>
2026-02-26 16:36:32 -0600

    "B:implemented AI"

--- main.py
+++ main.py
@@ -1,3 +1,13 @@
+import os
+from dotenv import load_dotenv
+from google import genai
+
+load_dotenv()
+api_key = os.environ.get("GEMINI_API_Key")
+client = genai.Client(api_key=api_key)
+
+if api_key is None:
+    raise Exception("RuntimeError")
 def main():
     print("Hello from ai-agent!")
 
