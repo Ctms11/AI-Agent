42e15004ce47e505557afad8946e8d685411b0f0
Ctms11 <clgraves2000@gmail.com>
2026-02-27 15:47:26 -0600

    C: Got AI API_KEY to Work

--- main.py
+++ main.py
@@ -3,14 +3,19 @@ from dotenv import load_dotenv
 from google import genai
 
 load_dotenv()
-api_key = os.environ.get("GEMINI_API_Key")
+api_key = os.environ.get("GEMINI_API_KEY")
+if api_key is None:
+    raise RuntimeError("RuntimeError: Key was not found")
 client = genai.Client(api_key=api_key)
 
-if api_key is None:
-    raise Exception("RuntimeError")
+response = client.models.generate_content(
+    model = 'gemini-2.5-flash', 
+    contents = 'Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.'
+)
+
 def main():
     print("Hello from ai-agent!")
-
+    print(response.text)
 
 if __name__ == "__main__":
     main()
