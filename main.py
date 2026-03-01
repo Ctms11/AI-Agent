4b56dc511970bc83eddf0fa249e9b6caeff6e8fa
Ctms11 <clgraves2000@gmail.com>
2026-03-01 01:31:28 -0600

    D: Refactor the main function and got the response and prompt token to show

--- main.py
+++ main.py
@@ -8,14 +8,23 @@ if api_key is None:
     raise RuntimeError("RuntimeError: Key was not found")
 client = genai.Client(api_key=api_key)
 
-response = client.models.generate_content(
-    model = 'gemini-2.5-flash', 
-    contents = 'Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.'
-)
+
 
 def main():
-    print("Hello from ai-agent!")
-    print(response.text)
+    my_prompt = "Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."   
+    response = client.models.generate_content(
+    model = 'gemini-2.5-flash', 
+    contents = my_prompt
+    )
+
+    if response.usage_metadata is None:
+        raise RuntimeError("RuntimeError: Usage metadata was not found")
+    else:
+        print(f"User prompt: {my_prompt}")
+        print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+        print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+        print("Response: ")
+        print(response.text)
 
 if __name__ == "__main__":
     main()
