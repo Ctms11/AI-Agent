64f1b82811f84e38749fc4aaa015cb83b0496e38
Ctms11 <clgraves2000@gmail.com>
2026-03-02 15:42:02 -0600

    E: Added argparse functionallity to grab user prompt instead of taking a hardcoded prompt

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import os
+import argparse
 from dotenv import load_dotenv
 from google import genai
 
@@ -11,16 +12,21 @@ client = genai.Client(api_key=api_key)
 
 
 def main():
-    my_prompt = "Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."   
+    parser = argparse.ArgumentParser(description="Chat Bot takes prompt from user")
+    parser.add_argument("user_prompt", type=str, help="Type in a prompt please")
+    args = parser.parse_args()
+
+    #my_prompt = "Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."   
     response = client.models.generate_content(
     model = 'gemini-2.5-flash', 
-    contents = my_prompt
+    contents = args.user_prompt
     )
 
+    
     if response.usage_metadata is None:
         raise RuntimeError("RuntimeError: Usage metadata was not found")
     else:
-        print(f"User prompt: {my_prompt}")
+        print(f"User prompt: {args}")
         print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
         print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
         print("Response: ")
