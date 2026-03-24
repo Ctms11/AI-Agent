999005b78958f574a2157793726721305b6f9a46
Ctms11 <clgraves2000@gmail.com>
2026-03-23 22:11:42 -0500

    P: Implement call_function to handle function calls and error responses

--- functions/call_function.py
+++ functions/call_function.py
@@ -7,3 +7,29 @@ from functions.get_file_content import schema_get_file_content
 available_functions = types.Tool(
     function_declarations=[schema_get_files_info, schema_get_file_content, schema_write_to_file, schema_run_python_file],
 )
+
+def call_function(function_call, verbose=False):
+    
+    if verbose:
+        print(f"Calling function: {function_call.name}({function_call.args})")
+    else:
+        print (f" - Calling function: {function_call.name}")
+
+    function_map = {
+    "get_files_info": schema_get_files_info,
+    "get_file_content": schema_get_file_content,
+    "write_to_file": schema_write_to_file,
+    "run_python_file": schema_run_python_file
+    }
+    args = dict(function_call.args) if function_call.args else {}
+    function_name = function_call.name or ""
+    if function_name not in function_map:
+        return types.Content(
+            role="tool",
+            parts=[
+                types.Part.from_function_response(
+                    name=function_name,
+                    response={"error": f"Unknown function: {function_name}"},
+                )
+            ],
+        )
