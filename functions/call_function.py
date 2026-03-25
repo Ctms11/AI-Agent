ec04224cf2d00b488b82d83224926464168c8e34
Ctms11 <clgraves2000@gmail.com>
2026-03-24 22:03:42 -0500

    Q: Refactor call_function to improve function mapping and add verbose logging; introduce config.py for constants

--- functions/call_function.py
+++ functions/call_function.py
@@ -1,8 +1,9 @@
 from google.genai import types
-from functions.get_files_info import schema_get_files_info 
-from functions.write_file import schema_write_to_file
-from functions.run_python_file import schema_run_python_file
-from functions.get_file_content import schema_get_file_content
+from functions.get_files_info import schema_get_files_info, get_files_info 
+from functions.write_file import schema_write_to_file, write_file
+from functions.run_python_file import schema_run_python_file, run_python_file
+from functions.get_file_content import schema_get_file_content, get_file_content
+from config import WORKING_DIR
 
 available_functions = types.Tool(
     function_declarations=[schema_get_files_info, schema_get_file_content, schema_write_to_file, schema_run_python_file],
@@ -11,17 +12,18 @@ available_functions = types.Tool(
 def call_function(function_call, verbose=False):
     
     if verbose:
-        print(f"Calling function: {function_call.name}({function_call.args})")
+        print(f" - Calling function: {function_call.name}({function_call.args})")    
     else:
         print (f" - Calling function: {function_call.name}")
 
     function_map = {
-    "get_files_info": schema_get_files_info,
-    "get_file_content": schema_get_file_content,
-    "write_to_file": schema_write_to_file,
-    "run_python_file": schema_run_python_file
+    "get_files_info": get_files_info,
+    "get_file_content": get_file_content,
+    "write_file": write_file,
+    "run_python_file": run_python_file,
     }
     args = dict(function_call.args) if function_call.args else {}
+    args["working_directory"] = WORKING_DIR
     function_name = function_call.name or ""
     if function_name not in function_map:
         return types.Content(
@@ -33,3 +35,14 @@ def call_function(function_call, verbose=False):
                 )
             ],
         )
+    else:
+        function_result = function_map[function_name](**args)
+        return types.Content(
+        role="tool",
+        parts=[
+            types.Part.from_function_response(
+                name=function_name,
+                response={"result": function_result},
+            )
+        ],
+    )
